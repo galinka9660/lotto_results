c58e6becbe4092bc1c4dd74b99029ff6bb116b0b
galinka9660 <galinka9660@yandex.ru>
2024-12-30 18:17:15 +0200

    Added home pages with different content for 2 types of users.

--- lotto_results/app.py
+++ lotto_results/app.py
@@ -1,14 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# defining probable users
+users = {
+    "admin": {"password": "12345", "role": "admin"},
+    "guest": {"password": "123", "role": "guest"}
+}
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    role = request.args.get('role', None)
+    return render_template("home.html", role=role)
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
+@app.route("/login", methods=['GET','POST'])
+def login():   
+    if request.method == 'POST':
+        username = request.form['username'] 
+        password = request.form['password']
+ 
+        for user in users:
+            if username == user and password == users[user]['password']:
+                role = users[user]['role']
+                return render_template("home.html", username=username, role=role)
+
+    return render_template("login.html") # shows the page with GET-request (form with login and Password) 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
